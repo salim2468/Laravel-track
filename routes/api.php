d8dc4993c985d4031236816f4f24a7ea0411f84e
salim2468 <salimmaharjan2468@gmail.com>
2023-09-08 15:14:06 +0545

    api controller moved to Contrller/Api folder

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\Api\UserController;
 
 /*
 |--------------------------------------------------------------------------
